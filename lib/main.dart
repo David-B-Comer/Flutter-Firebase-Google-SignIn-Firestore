fa25ac084da064c48cb5a4ffeaa09b99f836bf7e
David-B-Comer <davec2233@gmail.com>
2020-06-18 12:04:15 -0400

    added user profile

--- lib/main.dart
+++ lib/main.dart
@@ -25,3 +25,31 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class UserProfile extends StatefulWidget {
+  @override
+  UserProfileState createState() => UserProfileState();
+}
+
+class UserProfileState extends State<UserProfile> {
+  Map<String, dynamic> _profile;
+  bool _loading = false;
+
+  @override
+  initState() {
+    super.initState();
+
+    // Subscriptions are created here
+    authService.profile.listen((state) => setState(() => _profile = state));
+
+    authService.loading.listen((state) => setState(() => _loading = state));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Container(padding: EdgeInsets.all(20), child: Text(_profile.toString())),
+      Text(_loading.toString())
+    ]);
+  }
+}
